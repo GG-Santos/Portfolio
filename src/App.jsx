d932f5f0d56f77a4006049e3e20f8ca8aa783105
Kirsten-Wabwab <ollerkirstendaphne@gmail.com>
2024-12-11 20:02:56 +0800

    <create>

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
       <NavMenu />
       <HackerConsole />
 
-      {/* <BibleVerse />
-      <AlertButtons /> */}
+      <BibleVerse />
+      <AlertButtons />
       <IconBar />
 
     </>
